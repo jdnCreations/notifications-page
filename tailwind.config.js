d0f142812529d7e7eedab7736e39ecfa2b272995
Jordan Needham <jdncreations@hotmail.com>
2023-07-14 15:52:19 +0800

    setup fonts & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {},
+    fontFamily: '',
+    colors: {
+      primaryRed: "hsl(1, 90%, 64%)",
+      primaryBlue: "hsl(219, 85%, 26%)",
+      neutralWhite: "hsl(0, 0%, 100%)",
+      neutralVeryLightGrayishBlue: "hsl(210, 60%, 98%)",
+      neutralLightGrayishBlue__1: "hsl(211, 68%, 94%)",
+      neutralLightGrayishBlue__2: "hsl(205, 33%, 90%)",
+      neutralGrayishBlue: "hsl(219, 14%, 63%)",
+      neutralDarkGrayishBlue: "hsl(219, 12%, 42%)",
+      neutralVeryDarkBlue: "hsl(224, 21%, 14%)",
+    }
   },
   plugins: [],
 }
